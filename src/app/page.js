24924ad91d98477ed0e5222114949554909b2392
Brian Almada <brianalexisalmada84@gmail.com>
2024-06-18 22:58:54 -0300

    Arreglar problema de respuesta json en la API

--- src/app/page.js
+++ src/app/page.js
@@ -1,28 +1,29 @@
 "use client";
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react';
 
 export default function Home() {
-  const [books, setBooks] = useState([])
+  const [books, setBooks] = useState([]);
+
   useEffect(() => {
-    fetch(`http://localhost:4000/books`)
-    .then((res) => {
-        res.json()
-    })
-    .then(({ok, data}) => {
-        if(ok) {
-          setBooks(data)
+    fetch('http://localhost:4000/books')
+      .then((res) => res.json())
+      .then(({ ok, data }) => {
+        if (ok) {
+          setBooks(data);
         }
-    })
-    .catch((err) => {
-        console.log(err)
-    })
-  }, [])
-  console.log(books)
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  console.log(books);
+
   return (
     <div>
       <h1>hello</h1>
-      {books.map(b => (
-        <div className="df aic jcsb">
+      {books.map((b) => (
+        <div key={b._id} className="df aic jcsb">
           <h1>{b.title}</h1>
           <p>{b.description}</p>
         </div>
