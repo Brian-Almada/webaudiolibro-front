ff7f6002956913fd16c4165ef690a6507c6944c4
Brian Almada <brianalexisalmada84@gmail.com>
2024-06-21 22:26:51 -0300

    agregar la propiedad 'priority' a la imagen

--- src/app/page.js
+++ src/app/page.js
@@ -37,6 +37,7 @@ export default function Home() {
                   alt={b.title}
                   width={356}
                   height={200}
+                  priority
                 />
               </div>
               <div>
