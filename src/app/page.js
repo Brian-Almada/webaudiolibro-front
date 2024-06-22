99e1285593bd7a864c318aec9386685a0846fd50
Brian Almada <brianalexisalmada84@gmail.com>
2024-06-21 21:05:34 -0300

    Agregar estilos css y cambiar de archivo css

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,5 @@
 "use client";
-import './page.module.css';
+import './globals.css';
 import { useEffect, useState } from 'react';
 import Image from 'next/image';
 import AudioPlayer from './audioPlayer';
@@ -23,34 +23,36 @@ export default function Home() {
   console.log(books);
 
   return (
-    <div>
-      <div className="df aic jcsb">
-        <h1>Audio Libros</h1>
-        <button>Iniciar Sesión</button>
-      </div>
+    <>
+      <div className="df fdc aic jcc">
+        <div className="df fdc p5 tac mb5">
+          <h1>Audio Libros</h1>
+          <button>Iniciar Sesión</button>
+        </div>
 
-      {books.map((b) => (
-        <div key={b._id} className="df aic jcsb fdc">
-          <div>
-            <Image
-              src={b.thumbnail}
-              alt={b.title}
-              width={356}
-              height={200}
-            />
-          </div>
-          <div>
-            <h1>{b.title}</h1>
-            <p>{b.description}</p>
-            <p>{b.author}</p>
+        {books.map((b) => (
+          <div key={b._id} className="df aic jcsb fdc">
             <div>
-              <AudioPlayer
-                src={b.audio}
+              <Image
+                src={b.thumbnail}
+                alt={b.title}
+                width={356}
+                height={200}
               />
             </div>
+            <div>
+              <h1>{b.title}</h1>
+              <p>{b.description}</p>
+              <p>{b.author}</p>
+              <div>
+                <AudioPlayer
+                  src={b.audio}
+                />
+              </div>
+            </div>
           </div>
-        </div>
-      ))}
-    </div>
+        ))}
+      </div>
+    </>
   );
 }
