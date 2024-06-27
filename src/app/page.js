b6426d19e2499a1161e52d51779a0b8051796555
Brian Almada <brianalexisalmada84@gmail.com>
2024-06-26 21:04:49 -0300

    agregar nueva sintaxis a la variable de entorno para vercel

--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,7 @@ import { useEffect, useState } from 'react';
 
 import BookCard from '../../components/BookCard';
 
-const BASE_BACKEND_URL = !!process.env.VERCEL_ENV ? process.env.NEXT_PUBLIC_BASE_BACKEND_URL : 'http://localhost:4000'
+const BASE_BACKEND_URL = !!process.env.NEXT_PUBLIC_VERCEL_ENV ? process.env.NEXT_PUBLIC_BASE_BACKEND_URL : 'http://localhost:4000'
 export default function Home() {
   const [books, setBooks] = useState([]);
 
